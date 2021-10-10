475945a325491dd65096c83e74a7522ac53daed8
Rachana Chag <rachanachag@gmail.com>
2021-10-11 02:02:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/angular-app-heroku/' }),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+
+//PATH LOCATION STARTEGY
+
+app.get('/*', function (req, res) {
+    const fullPath = path.join(__dirname + '/dist/index.html');
+    console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
+
+console.log('Server started running..');
