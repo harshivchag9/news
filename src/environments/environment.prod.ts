15495aeceb07cf9657041153e62ba2f40d8b880c
Rachana Chag <rachanachag@gmail.com>
2021-11-02 23:29:44 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   country: 'IN',
-  NewsAPI_URL: 'https://newsapi.org/v2/top-headlines',
-  NewsAPI_KEY: '54ed1dcef0994aa5b1714a6e97728978',
+  NewsAPI_URL: 'https://newsapi.harshivchag.me',
+  // NewsAPI_URL: 'https://newsapi.org/v2/top-headlines',
+  NewsAPI_KEY: '77c0314a614c44edb1491183e4d58d99',
   LocationAPI_URL: 'https://ipinfo.io/json?token=512fe38d5c8c4b'
 };
